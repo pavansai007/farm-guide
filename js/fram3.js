1d898ec848f29d2711d3840a254c14cac20d1b9f
pavansai007 <49784494+pavansai007@users.noreply.github.com>
2020-11-01 07:22:10 +0530

    Add files via upload

---
+++ js/fram3.js
@@ -0,0 +1,45 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+/*---menu bar----*/
+document.querySelector('.nav-icon').addEventListener('click', function()  {
+
+document.querySelector('.menu').classList.add('opened');
+document.querySelector('.menu').classList.remove('closed');
+
+document.querySelector('.close').style.display = 'block';
+
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+
+document.querySelector('.menu').classList.remove('opened');
+document.querySelector('.menu').classList.add('closed');
+
+document.querySelector('.close').style.display = 'none';
+
+
+});
